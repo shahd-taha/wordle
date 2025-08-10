09003bf2e6670963be4a1213a853665e28408a87
shahd-taha <shahddtahaaa@gmail.com>
2025-08-11 00:27:51 +0300

    implement a function that checks whether or not the guess entered is in the dataset

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ def enter_word():
         letter=(input(f"Enter letter no {i+1}"))
         letters.append(letter)
     return "".join(letters) # to return the word as a string
+
+def check_guess(file_name,guess):
+    with open(file_name, 'r') as file:
+        dataset = file.read().splitlines()
+        if guess in dataset:
+            return True
+        else:
+            return False
