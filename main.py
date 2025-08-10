4bd50e97c008791b91150d3a0099445a5edf4a64
shahd-taha <shahddtahaaa@gmail.com>
2025-08-11 00:25:23 +0300

    implement a function that takes word guess from user

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ def random_word(file_name):
     with open(file_name,'r') as file:
         dataset = file.readlines()
         return random.choice(dataset).strip()
+    
+def enter_word():
+    letters=[]
+    for i in range(5): # 5 letters
+        letter=(input(f"Enter letter no {i+1}"))
+        letters.append(letter)
+    return "".join(letters) # to return the word as a string
